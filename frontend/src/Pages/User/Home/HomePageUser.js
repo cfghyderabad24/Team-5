a123f4ace9b4ffe5ab44dac71980e310d680962a
abajpai2004 <akarshbajpai1997@gmail.com>
2024-06-23 00:12:50 +0530

    deleting neccessary docs

--- frontend/src/Pages/User/Home/HomePageUser.js
+++ frontend/src/Pages/User/Home/HomePageUser.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import HomepageCarousel from "../../../Components/HomePageCarousel";
 import NavigationBar from "../NavigationBar/NavigationBar";
 
 // Import the NavigationBar component
